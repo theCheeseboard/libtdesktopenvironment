c40db545e8514ab24be5bc653fe7220cbeac937d
Victor Tran <vicr12345@gmail.com>
2022-02-09 10:17:01 +0000

    Added translation using Weblate (Vietnamese)

---
+++ lib/translations/vi.ts
@@ -0,0 +1,247 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" sourcelanguage="en_US" language="vi">
+<context>
+    <name>BackgroundSelectionModel</name>
+    <message>
+        <location filename="../Background/backgroundselectionmodel.cpp" line="73"/>
+        <source>Community Backgrounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Background/backgroundselectionmodel.cpp" line="74"/>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DesktopAccessibility</name>
+    <message>
+        <location filename="../Wm/desktopaccessibility.cpp" line="29"/>
+        <source>Sticky Keys</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DesktopUPower</name>
+    <message>
+        <location filename="../UPower/desktopupower.cpp" line="86"/>
+        <source>Power Stretch On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../UPower/desktopupower.cpp" line="100"/>
+        <source>%n more batteries</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>DesktopUPowerDevice</name>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="91"/>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="96"/>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="100"/>
+        <source>About %1% remaining</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="118"/>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="138"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="119"/>
+        <source>AC Power</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="120"/>
+        <source>Battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="121"/>
+        <source>Uninterruptable Power Supply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="122"/>
+        <source>Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="123"/>
+        <source>Mouse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="124"/>
+        <source>Keyboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="125"/>
+        <source>PDA</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="126"/>
+        <source>Phone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="139"/>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="151"/>
+        <source>Charging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="140"/>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="158"/>
+        <source>Discharging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="141"/>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="142"/>
+        <source>Fully Charged</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="143"/>
+        <source>Pending Charge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="144"/>
+        <source>Pending Discharge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="153"/>
+        <source>Charging · %1 until full</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../UPower/desktopupowerdevice.cpp" line="160"/>
+        <source>Discharging · %1 until empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../SystemSlide/private/slidepulseaudiomonitor.cpp" line="49"/>
+        <source>theShell Screen Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SlideMprisController</name>
+    <message>
+        <location filename="../SystemSlide/private/slidempriscontroller.cpp" line="60"/>
+        <source>Select Media Player</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SlidePulseaudioMonitor</name>
+    <message>
+        <location filename="../SystemSlide/private/slidepulseaudiomonitor.cpp" line="100"/>
+        <source>Lock Screen Visualisation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SlideQuickSettings</name>
+    <message>
+        <location filename="../SystemSlide/private/slidequicksettings.ui" line="61"/>
+        <source>QUICK SETTINGS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/private/slidequicksettings.ui" line="172"/>
+        <source>QUIET MODE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/private/slidequicksettings.ui" line="184"/>
+        <source>Sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/private/slidequicksettings.ui" line="200"/>
+        <source>Critical Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/private/slidequicksettings.ui" line="216"/>
+        <source>No Notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/private/slidequicksettings.ui" line="232"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/private/slidequicksettings.cpp" line="41"/>
+        <source>Flight Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemSlide</name>
+    <message>
+        <location filename="../SystemSlide/systemslide.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/systemslide.ui" line="253"/>
+        <source>BACKGROUND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/systemslide.ui" line="265"/>
+        <location filename="../SystemSlide/systemslide.ui" line="272"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/systemslide.cpp" line="263"/>
+        <source>Critical Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/systemslide.cpp" line="267"/>
+        <source>No Notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/systemslide.cpp" line="271"/>
+        <source>Mute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SystemSlide/systemslide.cpp" line="323"/>
+        <source>by %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>X11Backend</name>
+    <message>
+        <location filename="../Wm/x11/x11backend.cpp" line="310"/>
+        <source>Desktop %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
