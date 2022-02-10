0169655faa3b40aa974e95ba8edb2bba7245cec1
owahai <meero65@gmail.com>
2022-02-10 14:30:36 +0000

    Translated using Weblate (Arabic (Saudi Arabia))
    Currently translated at 100.0% (39 of 39 strings)
    
    Translation: libtdesktopenvironment/libtdesktopenvironment Core
    Translate-URL: https://translate.vicr123.com/projects/libtdesktopenvironment/libtdesktopenvironment-core/ar_SA/

--- lib/translations/ar_SA.ts
+++ lib/translations/ar_SA.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../Background/backgroundselectionmodel.cpp" line="73"/>
         <source>Community Backgrounds</source>
-        <translation type="unfinished"></translation>
+        <translation>خلفيات من مشاركات المجتمع</translation>
     </message>
     <message>
         <location filename="../Background/backgroundselectionmodel.cpp" line="74"/>
         <source>Custom</source>
-        <translation type="unfinished"></translation>
+        <translation>اختيارك العادة</translation>
     </message>
 </context>
 <context>
@@ -19,7 +19,7 @@
     <message>
         <location filename="../Wm/desktopaccessibility.cpp" line="29"/>
         <source>Sticky Keys</source>
-        <translation type="unfinished"></translation>
+        <translation>مفاتيح لزجة</translation>
     </message>
 </context>
 <context>
@@ -27,13 +27,18 @@
     <message>
         <location filename="../UPower/desktopupower.cpp" line="86"/>
         <source>Power Stretch On</source>
-        <translation type="unfinished"></translation>
+        <translation>تمدد القوة التشغيل</translation>
     </message>
     <message numerus="yes">
         <location filename="../UPower/desktopupower.cpp" line="100"/>
         <source>%n more batteries</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>٪ n بطاريات أخرى</numerusform>
+            <numerusform>٪ n بطاريات أخرى</numerusform>
+            <numerusform>٪ n بطاريات أخرى</numerusform>
+            <numerusform>٪ n بطاريات أخرى</numerusform>
+            <numerusform>٪ n بطاريات أخرى</numerusform>
+            <numerusform>٪ n بطاريات أخرى</numerusform>
         </translation>
     </message>
 </context>
@@ -44,95 +49,95 @@
         <location filename="../UPower/desktopupowerdevice.cpp" line="96"/>
         <location filename="../UPower/desktopupowerdevice.cpp" line="100"/>
         <source>About %1% remaining</source>
-        <translation type="unfinished"></translation>
+        <translation>حوالي٪ 1٪ متبقي</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="118"/>
         <location filename="../UPower/desktopupowerdevice.cpp" line="138"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>غير معروف</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="119"/>
         <source>AC Power</source>
-        <translation type="unfinished"></translation>
+        <translation>تيار مستمر</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="120"/>
         <source>Battery</source>
-        <translation type="unfinished"></translation>
+        <translation>البطارية</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="121"/>
         <source>Uninterruptable Power Supply</source>
-        <translation type="unfinished"></translation>
+        <translation>تيار طاقة غير المنقطع</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="122"/>
         <source>Display</source>
-        <translation type="unfinished"></translation>
+        <translation>شاشه</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="123"/>
         <source>Mouse</source>
-        <translation type="unfinished"></translation>
+        <translation>الفأر</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="124"/>
         <source>Keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>لوحة المفاتيح</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="125"/>
         <source>PDA</source>
-        <translation type="unfinished"></translation>
+        <translation>المساعد الشخصي الرقمي</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="126"/>
         <source>Phone</source>
-        <translation type="unfinished"></translation>
+        <translation>هاتف</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="139"/>
         <location filename="../UPower/desktopupowerdevice.cpp" line="151"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>الشحن</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="140"/>
         <location filename="../UPower/desktopupowerdevice.cpp" line="158"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>التفريغ</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="141"/>
         <source>Empty</source>
-        <translation type="unfinished"></translation>
+        <translation>فاضي</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="142"/>
         <source>Fully Charged</source>
-        <translation type="unfinished"></translation>
+        <translation>مشحون بالكامل</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="143"/>
         <source>Pending Charge</source>
-        <translation type="unfinished"></translation>
+        <translation>في انتظار الشحن</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="144"/>
         <source>Pending Discharge</source>
-        <translation type="unfinished"></translation>
+        <translation>في انتظار التفريغ</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="153"/>
         <source>Charging · %1 until full</source>
-        <translation type="unfinished"></translation>
+        <translation>الشحن ·٪ 1 حتى الامتلاء</translation>
     </message>
     <message>
         <location filename="../UPower/desktopupowerdevice.cpp" line="160"/>
         <source>Discharging · %1 until empty</source>
-        <translation type="unfinished"></translation>
+        <translation>تفريغ ·٪ 1 حتى الامتلاء</translation>
     </message>
 </context>
 <context>
@@ -140,7 +145,7 @@
     <message>
         <location filename="../SystemSlide/private/slidepulseaudiomonitor.cpp" line="49"/>
         <source>theShell Screen Lock</source>
-        <translation type="unfinished"></translation>
+        <translation>قفل الشاشة لtheShell</translation>
     </message>
 </context>
 <context>
@@ -148,7 +153,7 @@
     <message>
         <location filename="../SystemSlide/private/slidempriscontroller.cpp" line="60"/>
         <source>Select Media Player</source>
-        <translation type="unfinished"></translation>
+        <translation>حدد مشغل السوط</translation>
     </message>
 </context>
 <context>
@@ -156,7 +161,7 @@
     <message>
         <location filename="../SystemSlide/private/slidepulseaudiomonitor.cpp" line="100"/>
         <source>Lock Screen Visualisation</source>
-        <translation type="unfinished"></translation>
+        <translation>تصور شاشة القفل</translation>
     </message>
 </context>
 <context>
@@ -164,37 +169,37 @@
     <message>
         <location filename="../SystemSlide/private/slidequicksettings.ui" line="61"/>
         <source>QUICK SETTINGS</source>
-        <translation type="unfinished"></translation>
+        <translation>الإعدادات سريعة</translation>
     </message>
     <message>
         <location filename="../SystemSlide/private/slidequicksettings.ui" line="172"/>
         <source>QUIET MODE</source>
-        <translation type="unfinished"></translation>
+        <translation>الوضع الهادئ</translation>
     </message>
     <message>
         <location filename="../SystemSlide/private/slidequicksettings.ui" line="184"/>
         <source>Sound</source>
-        <translation type="unfinished"></translation>
+        <translation>الصوط</translation>
     </message>
     <message>
         <location filename="../SystemSlide/private/slidequicksettings.ui" line="200"/>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>المهمه فقط</translation>
     </message>
     <message>
         <location filename="../SystemSlide/private/slidequicksettings.ui" line="216"/>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>لا توجد اي إشعارات</translation>
     </message>
     <message>
         <location filename="../SystemSlide/private/slidequicksettings.ui" line="232"/>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>كتم الصوت</translation>
     </message>
     <message>
         <location filename="../SystemSlide/private/slidequicksettings.cpp" line="41"/>
         <source>Flight Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>وضع الطيران</translation>
     </message>
 </context>
 <context>
@@ -202,27 +207,27 @@
     <message>
         <location filename="../SystemSlide/systemslide.ui" line="252"/>
         <source>BACKGROUND</source>
-        <translation type="unfinished"></translation>
+        <translation>الخلفية</translation>
     </message>
     <message>
         <location filename="../SystemSlide/systemslide.cpp" line="263"/>
         <source>Critical Only</source>
-        <translation type="unfinished"></translation>
+        <translation>المهمه فقط</translation>
     </message>
     <message>
         <location filename="../SystemSlide/systemslide.cpp" line="267"/>
         <source>No Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>لا توجد اي إشعارات</translation>
     </message>
     <message>
         <location filename="../SystemSlide/systemslide.cpp" line="271"/>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>كتم الصوت</translation>
     </message>
     <message>
         <location filename="../SystemSlide/systemslide.cpp" line="323"/>
         <source>by %1</source>
-        <translation type="unfinished"></translation>
+        <translation>بي %1</translation>
     </message>
 </context>
 <context>
@@ -230,7 +235,7 @@
     <message>
         <location filename="../Wm/x11/x11backend.cpp" line="310"/>
         <source>Desktop %1</source>
-        <translation type="unfinished"></translation>
+        <translation>سطح المكتب %1</translation>
     </message>
 </context>
 </TS>
